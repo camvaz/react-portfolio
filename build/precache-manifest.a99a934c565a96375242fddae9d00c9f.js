8fccea6d705747c0a3eb20166db809b0c42428b8
camvaz <camposvazquezv@gmail.com>
2020-04-01 13:49:52 -0600

    build added for netlify deployment

---
+++ build/precache-manifest.a99a934c565a96375242fddae9d00c9f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6220df5ea54646dbdc44cfe39027d1ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e813b2935d1764064fb2",
+    "url": "/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "eba2c61c0afbad2eb61b",
+    "url": "/static/css/main.d461a7b0.chunk.css"
+  },
+  {
+    "revision": "e813b2935d1764064fb2",
+    "url": "/static/js/2.9136b09b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9136b09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba2c61c0afbad2eb61b",
+    "url": "/static/js/main.ba58f4aa.chunk.js"
+  },
+  {
+    "revision": "3a41f2cc0023acc00ddf",
+    "url": "/static/js/runtime-main.de94feee.js"
+  },
+  {
+    "revision": "cdb1ea9d6f2ac9ff312ffa42a8091d00",
+    "url": "/static/media/deployment.cdb1ea9d.svg"
+  },
+  {
+    "revision": "b2d4e916a3b45256aa322a2de1697f1a",
+    "url": "/static/media/github.b2d4e916.svg"
+  },
+  {
+    "revision": "9f3108f4cd1d206d3c2325b4c69f7803",
+    "url": "/static/media/twitter.9f3108f4.svg"
+  },
+  {
+    "revision": "b588b3beb01174c6b500273d8f4c075e",
+    "url": "/static/media/undraw-webdev.b588b3be.png"
+  }
+]);
